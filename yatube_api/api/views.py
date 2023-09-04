bffed06c569da8dc0780712232a7fb26b7211ae3
Aleksandr Dymchenko <aleksandrdymchenko@SPB-HQ-NB-1277.local>
2023-09-04 17:49:12 +0300

    new fixes

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,115 +1,36 @@
-from rest_framework import viewsets, permissions
-from posts.models import Post, Group, Comment
+from rest_framework import viewsets
+from posts.models import Post, Group
 from .serializeres import PostSerializer, CommentSerializer, GroupSerializer
-from rest_framework.decorators import action
 from django.shortcuts import get_object_or_404
-from rest_framework import status
-from rest_framework.response import Response
+from api.permissions import IsAuthorOrReadOnly
+from rest_framework.permissions import IsAuthenticated
+
+PERMISSIONS = [IsAuthenticated, IsAuthorOrReadOnly]
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = PERMISSIONS
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-    def perform_destroy(self, instance):
-        if instance.author != self.request.user:
-            return Response(status=status.HTTP_403_FORBIDDEN)
-        instance.delete()
-
-    def update(self, request, *args, **kwargs):
-        instance = self.get_object()
-        if instance.author != self.request.user:
-            return Response(status=status.HTTP_403_FORBIDDEN)
-        return super().update(request, *args, **kwargs)
-
-    def destroy(self, request, *args, **kwargs):
-        instance = self.get_object()
-
-        if instance.author != request.user:
-            return Response(status=status.HTTP_403_FORBIDDEN)
-
-        self.perform_destroy(instance)
-
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
 
 class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = PERMISSIONS
 
-    def create(self, request, *args, **kwargs):
-        post = get_object_or_404(Post, pk=kwargs['post_pk'])
-        serializer = self.get_serializer(data=request.data)
-
-        if serializer.is_valid():
-            serializer.save(author=request.user, post=post)
-            return Response(
-                serializer.data,
-                status=status.HTTP_201_CREATED
-            )
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get_post(self):
+        return get_object_or_404(Post, pk=self.kwargs.get('post_pk'))
 
     def get_queryset(self):
-        post_id = self.kwargs['post_pk']
-
-        return Comment.objects.filter(post=post_id)
+        return self.get_post().comments.all()
 
     def perform_create(self, serializer):
-        post_id = self.kwargs['post_pk']
-        post = get_object_or_404(Post, pk=post_id)
-        serializer.save(author=self.request.user, post=post)
-
-    def update(self, request, *args, **kwargs):
-        comment = get_object_or_404(Comment, pk=kwargs['pk'])
-
-        if comment.author != request.user:
-            return Response(status=status.HTTP_403_FORBIDDEN)
-
-        serializer = self.get_serializer(
-            comment, data=request.data,
-            partial=request.method == 'PATCH'
-        )
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def destroy(self, request, *args, **kwargs):
-        comment = get_object_or_404(Comment, pk=kwargs['pk'])
-
-        if comment.author != request.user:
-            return Response(status=status.HTTP_403_FORBIDDEN)
-
-        comment.delete()
-
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-    def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        serializer = CommentSerializer(queryset, many=True)
-
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        serializer.save(author=self.request.user, post=self.get_post())
 
 
-class GroupViewSet(viewsets.ModelViewSet):
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def retrieve(self, request, *args, **kwargs):
-        if not request.user.is_authenticated:
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
-
-        return super().retrieve(request, *args, **kwargs)
-
-    def create(self, request, *args, **kwargs):
-        if not request.user.is_authenticated:
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
-
-        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
